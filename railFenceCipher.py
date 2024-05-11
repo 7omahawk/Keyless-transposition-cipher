4d30f42be0e5d1477017fcbad9160cfcea38e1c7
7omahawk <ahulr.official@gmial.com>
2024-05-11 20:36:38 +0600

    added decryption method

--- railFenceCipher.py
+++ railFenceCipher.py
@@ -1,6 +1,9 @@
+# making rail fence cipher aka keyless transposition cipher encryption and decryption using python
 
 import sys
+import math
 
+# the encryption function of rail fence cipher
 def encryption(userInput):
 
     userInput = userInput.replace(" ", "")
@@ -9,20 +12,40 @@ def encryption(userInput):
     secondRow = ""
 
     for i in range(sizeOfInput):
-
         if i % 2 == 0:
             firstRow = firstRow + userInput[i]
         else: 
             secondRow = secondRow + userInput[i]
 
     cipher = firstRow + secondRow
+    print(f"The encrypted message is: {cipher}")
 
-    print(f"The encrypted text is: {cipher}")
 
+# the encryption function of rail fence cipher
 def decryption(userInput):
-    hello = "will be update here"
+    
+    userInput = userInput.replace(" ", "")
+    sizeOfInput = len(userInput)
+    print(sizeOfInput)
 
+    divid = math.ceil(sizeOfInput / 2)
+    temp = divid   # the temp variable is use for the number of loop running
+    plaintext = ""
 
+    if sizeOfInput % 2 == 0:   # when the length of input size is even this condition will work
+        for i in range(temp):
+            plaintext = plaintext + userInput[i]
+            if i < temp:
+                plaintext = plaintext + userInput[divid]
+            divid += 1
+    else:
+        for i in range(temp):  # when the length of input size is odd this condition will work
+            plaintext = plaintext + userInput[i]
+            if i < temp-1:
+                plaintext = plaintext + userInput[divid]
+            divid += 1
+    
+    print(f"The decrypted message is: {plaintext}")
 
 
 while(True):
